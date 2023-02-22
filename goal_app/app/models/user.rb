cfda350244d2f685c9f3345828bc6374ba0c984f
Steven Notovitz <sjn39@cornell.edu>
2023-02-22 16:03:55 -0500

    Add password=

--- goal_app/app/models/user.rb
+++ goal_app/app/models/user.rb
@@ -12,4 +12,13 @@
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
 
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 end
--- goal_app/spec/models/user_spec.rb
+++ goal_app/spec/models/user_spec.rb
@@ -15,4 +15,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_presence_of(:session_token) }
+  
+  it { should allow_value(nil).for(:password) }
+  it { should validate_length_of(:password).is_at_least(6) }
 end
